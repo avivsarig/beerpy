f1df817a41b0980ef84ead30909fa5219f74fb8b
avivsarig <avivs15@gmail.com>
2023-01-23 21:32:22 +0200

    created users.py file in routers folder, added the router to main.py
     Changes to be committed:
    	modified:   backend/main.py
    	new file:   backend/routers/users.py

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,7 @@ from backend.database import db
 app = FastAPI()
 app.include_router(beers.router)
 app.include_router(orders.router)
+app.include_router(users.router)
 
 if db.connect():
     print("Database connection established")
