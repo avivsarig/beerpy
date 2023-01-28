2f25eb6202f910870d5177155537bffd39bbdd8f
avivsarig <avivs15@gmail.com>
2023-01-28 17:56:38 +0200

    add get_by_id, post, delete and update to Stock router

--- routers/stock.py
+++ routers/stock.py
@@ -7,17 +7,20 @@ from utils.query_to_filters import query_to_filters
 
 router = APIRouter(prefix="/stock")
 
+
 @router.get("/")
 async def get_stock(request: Request):
     query = Stock.select()
     filters = query_to_filters(request["query_string"])
     if filters != []:
-        
+
         for filter in filters:
             if filter["field"] == "beer_id":
                 query = query.where(Stock.beer_id == filter["value"])
             if filter["field"] == "date_of_arrival":
-                query = query.where(filter["op"](Stock.date_of_arrival, filter["value"]))
+                query = query.where(
+                    filter["op"](Stock.date_of_arrival, filter["value"])
+                )
             if filter["field"] == "qty_in_stock":
                 query = query.where(filter["op"](Stock.qty_in_stock, filter["value"]))
 
@@ -27,3 +30,50 @@ async def get_stock(request: Request):
 
     res["qty"] = len(res["results"])
     return res
+
+
+@router.get("/{stock_id}")
+async def get_stock_by_id(stock_id):
+    query = Stock.select().where(Stock.id == stock_id)
+    if not query.exists():
+        raise HTTPException(status_code=404, detail="Stock not found")
+    else:
+        return query.get()
+
+
+@router.post("/")
+async def create_stock(request: Request):
+    body = await request.json()
+    try:
+        with db.atomic():
+            res = Stock.create(**body)
+            return res
+    except IntegrityError as e:
+        print(e, flush=True)
+        return Response()
+
+
+@router.delete("/{stock_id}")
+async def delete_stock(stock_id):
+    query = Stock.selet().where(Stock.id == stock_id)
+    if not query.exists():
+        raise HTTPException(status_code=404, detail="Stock not found")
+    else:
+        with db.atomic():
+            return Stock.delete().where(Stock.id == stock_id).execute()
+
+
+@router.put("/{stock_id}")
+async def update_stock(request: Request, stock_id: int):
+    body = await request.json()
+    try:
+        query = Stock.select().where(Stock.id == stock_id)
+        if not query.exists():
+            raise HTTPException(status_code=404, detail="Beer not found")
+        else:
+            with db.atomic():
+                Stock.update(**body).where(Stock.id == stock_id).execute()
+
+    except IntegrityError as e:
+        print(e, flush=True)
+        return Response()
