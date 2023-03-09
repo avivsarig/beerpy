e96f717e109921d25249b86f1b3c813f952cba59
AvivSarig <avivs15@gmail.com>
2023-03-09 10:33:25 +0200

    new line in messages, peewee location edited
     Changes to be committed:
    	deleted:    main.py
    	modified:   tests/test_users_router.py

--- tests/test_users_router.py
+++ tests/test_users_router.py
@@ -2,5 +2,7 @@ from fastapi import Request
 import requests
 
 
-def check_get_user(request: Request) -> dict:
-    return requests.get(request)
+def get_request(url: str, params: dict):
+    return requests.get(url, params)
+
+print(type(get_request))
