06bca6e3bd41f75555d195f1fd9bccf58ba8adf3
AvivSarig <avivs15@gmail.com>
2023-03-10 10:46:56 +0200

    gitignore: add mypy and pytest, trying tests at pytest

--- tests/test_users_router.py
+++ tests/test_users_router.py
@@ -1,8 +1,16 @@
 from fastapi import Request
 import requests
 
+root = "http://127.0.0.1:8000"
 
-def get_request(url: str, params: dict):
-    return requests.get(url, params)
 
-print(type(get_request))
+def get_request(url: str):
+    return requests.get(root + url).text
+
+
+def test_root():
+    assert get_request("/") == '{"detail":"Not Found"}'
+
+
+def test_empty_users():
+    assert get_request("/users") == '{"qty":0,"results":[]}'
