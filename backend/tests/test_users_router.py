77f45c4cc7900ca0e526077ba089aa16358045bd
AvivSarig <avivs15@gmail.com>
2023-03-10 10:46:56 +0200

    gitignore: add mypy and pytest, trying tests at pytest
     Changes to be committed:
    	modified:   .gitignore
    	new file:   backend/tests/test_users_router.py
    
     Changes to be committed:
    	modified:   .gitignore
    	new file:   backend/tests/test_users_router.py

---
+++ backend/tests/test_users_router.py
@@ -0,0 +1,13 @@
+from fastapi import Request
+import requests
+
+
+<<<<<<< HEAD
+def get_request(url: str, params: dict):
+    return requests.get(url, params)
+
+print(type(get_request))
+=======
+def check_get_user(request: Request) -> dict:
+    return requests.get(request)
+>>>>>>> 7c5462f (tests dir added)
