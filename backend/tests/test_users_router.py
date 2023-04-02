d5521e621c227bce766f844c083437be133b1059
AvivSarig <avivs15@gmail.com>
2023-04-02 20:57:56 +0300

    add tests

--- backend/tests/test_users_router.py
+++ backend/tests/test_users_router.py
@@ -131,9 +131,7 @@ class Test_get_user:
                 f"INSERT INTO users (name, email, password, address, phone) VALUES ({data_string}) RETURNING id;"
             ).fetchall()[0][0]
         response = client.get(f"/users/{id}")
-        res_correct = {
-            key: data[key] for key in ["name", "email", "address", "phone"]
-        }
+        res_correct = {key: data[key] for key in ["name", "email", "address", "phone"]}
         res_correct["id"] = id
         assert response.json()["__data__"] == res_correct
 
@@ -154,11 +152,128 @@ class Test_get_all:
     def test_get_all(self, clean_db):
         with db.atomic():
             for i in range(10):
-                data = create_payload(f'test_get_all{i}', f'test_email{i}', f'test_pw{i}', f'test_address{i}', f'test_phone{i}')
+                data = create_payload(
+                    f"test_get_all{i}",
+                    f"test_email{i}",
+                    f"test_pw{i}",
+                    f"test_address{i}",
+                    f"test_phone{i}",
+                )
                 data_string = payload_to_string(data)
-                db.execute_sql(f"INSERT INTO users (name, email, password, address, phone) VALUES ({data_string});")
+                db.execute_sql(
+                    f"INSERT INTO users (name, email, password, address, phone) VALUES ({data_string});"
+                )
 
         query = User.select(User.id, User.name, User.email, User.address, User.phone)
         response = client.get("/users/")
-        print(response.json()['results'])
-        assert query == response
+        assert response.json()["qty"] == 10
+
+        for user_dict, response_dict in zip(query.dicts(), response.json()["results"]):
+            for attr in ["id", "name", "email", "address", "phone"]:
+                assert (
+                    user_dict[attr] == response_dict[attr]
+                ), f"{attr} incorrect"
+        assert len(query) == len(response.json()["results"])
+
+    def test_get_all_ok_code(self, clean_db):
+        response = client.get("/users/")
+        assert response.status_code == 200
+
+
+class Test_update_user:
+    def test_update_user_content(self, clean_db):
+        with db.atomic():
+            id = db.execute_sql(
+                "INSERT INTO users (name, email, password, address, phone) VALUES ('test_update', 'test_email', 'test_pw', 'test_address', 'test_phone') RETURNING id;"
+            ).fetchall()[0][0]
+
+        updated_payload = create_payload(
+            name="updated_test_user",
+            email="updated_test_email",
+            password="updated_test_pw",
+            address="updated_test_address",
+            phone="updated_test_phone",
+        )
+        response = client.put(f"/users/{id}", json=updated_payload)
+
+        query = User.select().where(User.id == id).dicts()
+        assert query == updated_payload
+
+    def test_update_user_ok_code(self, clean_db):
+        with db.atomic():
+            id = db.execute_sql(
+                "INSERT INTO users (name, email, password, address, phone) VALUES ('test_update_ok_code', 'test_email', 'test_pw', 'test_address', 'test_phone') RETURNING id;"
+            ).fetchall()[0][0]
+
+        updated_payload = create_payload(
+            name="updated_test_user",
+            email="updated_test_email",
+            password="updated_test_pw",
+            address="updated_test_address",
+            phone="updated_test_phone",
+        )
+        response = client.put(f"/users/{id}", json=updated_payload)
+
+        assert response.status_code == 200
+
+    @pytest.mark.parametrize(
+        "name, email, password",
+        [
+            (None, "updated_test_email", "updated_test_pw"),
+            ("updated_test_user", None, "updated_test_pw"),
+            ("updated_test_user", "updated_test_email", None),
+        ],
+    )
+    def test_update_user_error_code(self, clean_db, name, email, password):
+        with db.atomic():
+            id = db.execute_sql(
+                "INSERT INTO users (name, email, password, address, phone) VALUES ('test_update_error_code', 'test_email', 'test_pw', 'test_address', 'test_phone') RETURNING id;"
+            ).fetchall()[0][0]
+
+        updated_payload = create_payload(name, email, password)
+        response = client.put(f"/users/{id}", json=updated_payload)
+
+        assert response.status_code == 400
+
+    def test_update_user_not_found(self, clean_db):
+        non_existent_id = 1
+        updated_payload = create_payload(
+            name="updated_test_user",
+            email="updated_test_email",
+            password="updated_test_pw",
+            address="updated_test_address",
+            phone="updated_test_phone",
+        )
+        response = client.put(f"/users/{non_existent_id}", json=updated_payload)
+
+        assert response.status_code == 404
+
+
+class Test_filter_users:
+    def test_get_all_with_filter(self, clean_db):
+        with db.atomic():
+            for i in range(10):
+                data = create_payload(
+                    f"test_get_all_filter{i}",
+                    f"test_email{i}",
+                    f"test_pw{i}",
+                    f"test_address{i}",
+                    f"test_phone{i}",
+                )
+                data_string = payload_to_string(data)
+                db.execute_sql(
+                    f"INSERT INTO users (name, email, password, address, phone) VALUES ({data_string});"
+                )
+
+
+            response = client.get("/users/", params={"name": "test_get_all_filter2"})
+
+            assert len(response.json()["results"]) == 1
+            assert (
+                response.json()["results"][0]["__data__"]["name"]
+                == "test_get_all_filter2"
+            )
+
+    def test_get_all_with_filter_ok_code(self, clean_db):
+        response = client.get("/users/", params={"name": "test_get_all_filter_ok_code"})
+        assert response.status_code == 200
