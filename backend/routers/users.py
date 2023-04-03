5c76de8375c55671156382b845bdbea704e23d51
AvivSarig <avivs15@gmail.com>
2023-04-03 14:24:50 +0300

    update_user will return code 200

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -74,17 +74,10 @@ async def update_user(request: Request, user_id):
         query = User.select().where(User.id == user_id)
         if not query.exists():
             raise HTTPException(status_code=404, detail="User not found\n")
-
-        user_from_db = query.get().__dict__["__data__"]
-        body = await request.json()
-
-        for key in user_from_db.keys():
-            if key not in body.keys():
-                body[key] = user_from_db[key]
-
-        with db.atomic():
-            User.update(**body).where(User.id == user_id).execute()
-            return Response(status_code=200)
+        else:
+            with db.atomic():
+                User.update(**body).where(User.id == user_id).execute()
+                return Response(status_code=200)
     except IntegrityError as e:
         code, message = response_from_error(e)
         raise HTTPException(status_code=code, detail=message)
