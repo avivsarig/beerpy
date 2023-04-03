f126f9dcede08ba5aed604dbde41776712481a5c
AvivSarig <avivs15@gmail.com>
2023-04-03 15:41:01 +0300

    fix update_user endpoint and create support for partial body on request

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -35,7 +35,7 @@ async def get_users(request: Request) -> dict:
 
 @router.get("/{user_id}")
 async def get_user_by_id(user_id):
-    query = User.select(User.id, User.name, User.email, User.address, User.phone)
+    query = User.select(User.id, User.name, User.email, User.address, User.phone).where(User.id == user_id)
     if not query.exists():
         raise HTTPException(status_code=404, detail="User not found\n")
     else:
@@ -68,15 +68,21 @@ async def delete_user(user_id):
 
 @router.put("/{user_id}")
 async def update_user(request: Request, user_id):
-    body = await request.json()
     try:
         query = User.select().where(User.id == user_id)
         if not query.exists():
             raise HTTPException(status_code=404, detail="User not found\n")
-        else:
-            with db.atomic():
-                User.update(**body).where(User.id == user_id).execute()
-                return Response(status_code=200)
+        
+        user_from_db = query.get().__dict__['__data__']
+        body = await request.json()
+        
+        for key in user_from_db.keys():
+            if key not in body.keys():
+                body[key] = user_from_db[key]
+        
+        with db.atomic():
+            User.update(**body).where(User.id == user_id).execute()
+            return Response(status_code=200)
     except IntegrityError as e:
         code, message = response_from_error(e)
         raise HTTPException(status_code=code, detail=message)
