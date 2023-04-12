d38a7788a160a9d1d3ef98e5e714a3e180903600
AvivSarig <avivs15@gmail.com>
2023-04-12 12:28:27 +0300

    in Beer model, price constraint change from positive to non-negative

--- backend/models.py
+++ backend/models.py
@@ -12,7 +12,7 @@ class Beer(BaseModel):
     name = CharField(60)
     style = CharField(50, null=True)
     abv = DecimalField(3, 1, constraints=[Check("abv>=0")])
-    price = DecimalField(6, 2, constraints=[Check("price>0")])
+    price = DecimalField(6, 2, constraints=[Check("price>=0")])
 
     class Meta:
         table_name = "beers"
