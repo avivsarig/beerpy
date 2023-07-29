6088b00a90191581f973f1cb192a287ec81c14d9
AvivSarig <avivs15@gmail.com>
2023-07-29 23:22:56 +0300

    Settings updated to receive os.getenv

--- backend/settings.py
+++ backend/settings.py
@@ -5,8 +5,8 @@ ORDERS_PAGE_LIMIT = int(os.getenv("BEER_PAGE_LIMIT", "20"))
 STOCK_PAGE_LIMIT = int(os.getenv("BEER_PAGE_LIMIT", "20"))
 USER_PAGE_LIMIT = int(os.getenv("BEER_PAGE_LIMIT", "20"))
 
-DATABASE = "beerpy"
-PGHOST = "localhost"
-PORT = "5432"
-USER = "aviv"
-PASSWORD = "AsPsql23"
+DATABASE = os.getenv("DATABASE", "beerpy")
+PGHOST = os.getenv("PGHOST", "localhost")
+PORT =  os.getenv("PORT", "5432")
+USER = os.getenv("USER", "aviv")
+PASSWORD = os.getenv("PASSWORD", "AsPsql23")
